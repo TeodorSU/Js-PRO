09aba60da3910bb1575a54ed9b020842af3456a5
TheodorSU <greyorgreen@mail.ru>
2021-02-16 23:14:46 +0300

    ДЗ к 1 уроку Продвинутый JS

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const data = [
+    { title: 'Notebook', id: 1, price: 2000 },
+    { title: 'Keyboard', id: 2, price: 200 },
+    { title: 'Mouse', id: 3, price: 100 },
+    { title: 'Gamepad', id: 4, price: 87 },
+];
+
+const renderProduct = ({title = 'Undefined', price}, img = 'https://placehold.it/250X200') => {
+    return `
+        <div class="product-item">
+            <img src="${img}" alt="${title}">
+            <div class="desc">
+                 <div class="desc__info">
+                    <h3>${title}</h3>
+                    <p>${price}</p>
+                </div>
+               <button class="btn_buy" type="button">Купить</button>
+            </div>      
+        </div>
+    `;
+};
+
+const render = (products) => {
+    document.querySelector('.products').innerHTML = products.map(item => renderProduct(item)).join('');
+};
+
+render(data);
